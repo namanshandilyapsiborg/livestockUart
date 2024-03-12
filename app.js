8fe0453531634aac6f5689474101b3575079aa8d
namanshandilyapsiborg <naman.shandilya@psiborg.in>
2024-03-12 15:44:18 +0530

    added local serve

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const fs = require('fs')
 const app = express()
 let port1 = 3001
 
- const { SerialPort } = require("serialport");
- const port = new SerialPort({ path: "/dev/ttyAMA0", baudRate: 2000000});
- const { ReadlineParser } = require("@serialport/parser-readline");
- const parser = port.pipe(new ReadlineParser({ delimiter: "\r\n" }));
+const { SerialPort } = require("serialport");
+const port = new SerialPort({ path: "/dev/ttyAMA0", baudRate: 2000000 });
+const { ReadlineParser } = require("@serialport/parser-readline");
+const parser = port.pipe(new ReadlineParser({ delimiter: "\r\n" }));
 
 
 
@@ -18,18 +18,18 @@ const client2 = mqtt.connect('mqtt://103.127.30.171');
 
 // Connect to the broker
 client.on('connect', () => {
-console.log('Publisher connect to MQTT broker');
+    console.log('Publisher connect to MQTT broker');
 
-// Subscribe to the topic
-client.subscribe('livestokeGateway', (err) => {
-if(!err){
-console.log('Subscribed to the topic');
+    // Subscribe to the topic
+    client.subscribe('livestokeGateway', (err) => {
+        if (!err) {
+            console.log('Subscribed to the topic');
 
-}else{
-console.log('Subscribe Failed', err);
-}
+        } else {
+            console.log('Subscribe Failed', err);
+        }
 
-});
+    });
 
 });
 
@@ -37,23 +37,23 @@ console.log('Subscribe Failed', err);
 // Connect to the broker
 client2.on('connect', () => {
     console.log('Publisher connect to Main Server MQTT broker');
-    
+
     // Subscribe to the topic
     client2.subscribe('livestokeGateway', (err) => {
-    if(!err){
-    console.log('Subscribed to the topic');
-    
-    }else{
-    console.log('Subscribe Failed', err);
-    }
-    
+        if (!err) {
+            console.log('Subscribed to the topic');
+
+        } else {
+            console.log('Subscribe Failed', err);
+        }
+
     });
-    
-    });
+
+});
 
 // Handle incomming message
 client.on('message', (topic, message) => {
-console.log(`Received message on topic '${topic}': ${message.toString()}`);
+    console.log(`Received message on topic '${topic}': ${message.toString()}`);
 
 });
 
@@ -61,26 +61,31 @@ console.log(`Received message on topic '${topic}': ${message.toString()}`);
 // Handle incomming message
 client2.on('message', (topic, message) => {
     console.log(`Received message on topic '${topic}': ${message.toString()}`);
-    
-    });
+
+});
 
 async function connectSerialPort() {
     try {
         parser.on("data", async (data) => {
             console.log("Data from port Device=====>", data);
-	client.publish('livestokeServer', data);
-    client2.publish('livestokeServer', data);
-});
+            client.publish('livestokeServer', data);
+            client2.publish('livestokeServer', data);
+        });
     } catch (error) {
         console.log(error, 'error from my side app')
     }
 }
 
 // this one is commented for i am not using uart 
- connectSerialPort();
+connectSerialPort();
+
+app.use(express.static(path.join(__dirname, "build"), { cacheControl: false }));
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(port1, () => {
     console.log(`Server is running on ${port1}`);
-	require("./firefoxOpen")();
+    require("./firefoxOpen")();
 
 })
